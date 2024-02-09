fc93eab5a9a9c6ebfb0d768cc575d2be7f6c5d75
topaze <topaze@nohlan.fr>
2024-02-08 22:05:31 -0500

    user scope, data mount

--- datagrip.sh
+++ datagrip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 xauth add :1 . ${XAUTH_TOKEN}
 version=2023.3.4
+cd /home/datagrip
 if [ ! -d "DataGrip-${version}" ]; then
     curl https://download-cdn.jetbrains.com/datagrip/datagrip-${version}.tar.gz --output datagrip-${version}.tar.gz
     tar xzvf datagrip-${version}.tar.gz
